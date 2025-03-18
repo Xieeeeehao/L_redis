739cd9e716087e299ebe76bf8255917794d31f7e
xiehao <xie_haooo@163.com>
2025-03-18 12:32:02 +0800

    Initial commit

---
+++ src/main/java/com/xiehao/LRedisApplication.java
@@ -0,0 +1,13 @@
+package com.xiehao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiehao/LRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiehao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
